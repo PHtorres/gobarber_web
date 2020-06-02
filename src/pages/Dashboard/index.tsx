20b42001a31f2e571357a904ead9b2122bb7f4b8
PHtorres <58959268+PHtorres@users.noreply.github.com>
2020-06-02 02:13:32 -0300

    Cirando lógica de redirecionamento para rotas autenticadas

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Dashboard: React.FC = () =>{
+   
     return(
         <h1>Hello Dashbord</h1>
     )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch} from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
 
 
 const Routes:React.FC = () =>{
@@ -10,6 +12,7 @@ const Routes:React.FC = () =>{
         <Switch>
             <Route path="/" exact component={SignIn}/>
             <Route path="/signup" component={SignUp}/>
+            <Route path="/dashboard" component={Dashboard} isPrivate/>
         </Switch>
     )
 }
