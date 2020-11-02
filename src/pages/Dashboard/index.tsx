5c96c38c1c28ab6db22db9059dfe88fd862548ed
Paulo Henrique Torres <paulohenriquetorresdev@gmail.com>
2020-11-01 23:51:34 -0300

    Criando section de agendamentos no dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,23 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useAuth } from '../../hooks/Auth';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+    Container,
+    Header,
+    HeaderContent,
+    Profile,
+    Content,
+    Schedule,
+    NextAppointment,
+    Section,
+    Appointment,
+    Calendar
+} from './styles';
 import logoImg from '../../assets/logo.svg';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 
 const Dashboard: React.FC = () => {
+
+    const [selectedDate, setSelectedDate] = useState(new Date());
 
     const { signOut, user } = useAuth();
 
@@ -25,6 +38,68 @@ const Dashboard: React.FC = () => {
                     </button>
                 </HeaderContent>
             </Header>
+            <Content>
+                <Schedule>
+                    <h1>Horários agendados</h1>
+                    <p>
+                        <span>Hoje</span>
+                        <span>Dia 06</span>
+                        <span>Segunda-feira</span>
+                    </p>
+                    <NextAppointment>
+                        <strong>Atendimento a seguir</strong>
+                        <div>
+                            <img src="https://avatars2.githubusercontent.com/u/7872329?s=460&u=f41fcb80dc9ce32a809caf9a6c4d9bf31c6ae11a&v=4"
+                                alt="Foto do agendamento" />
+                            <strong>Nome do usuário</strong>
+                            <span>
+                                <FiClock />
+                            09:00
+                        </span>
+                        </div>
+                    </NextAppointment>
+                    <Section>
+                        <strong>Manhã</strong>
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                11:00
+                            </span>
+                            <div>
+                                <img src="https://avatars2.githubusercontent.com/u/7872329?s=460&u=f41fcb80dc9ce32a809caf9a6c4d9bf31c6ae11a&v=4"
+                                    alt="Foto do agendamento" />
+                                <strong>Nome do usuário 2</strong>
+                            </div>
+                        </Appointment>
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+                            <div>
+                                <img src="https://avatars2.githubusercontent.com/u/7872329?s=460&u=f41fcb80dc9ce32a809caf9a6c4d9bf31c6ae11a&v=4"
+                                    alt="Foto do agendamento" />
+                                <strong>Nome do usuário</strong>
+                            </div>
+                        </Appointment>
+                    </Section>
+                    <Section>
+                        <strong>Tarde</strong>
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                14:00
+                            </span>
+                            <div>
+                                <img src="https://avatars2.githubusercontent.com/u/7872329?s=460&u=f41fcb80dc9ce32a809caf9a6c4d9bf31c6ae11a&v=4"
+                                    alt="Foto do agendamento" />
+                                <strong>Nome do usuário 3</strong>
+                            </div>
+                        </Appointment>
+                    </Section>
+                </Schedule>
+                <Calendar></Calendar>
+            </Content>
         </Container>
     )
 }
